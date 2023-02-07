88881562c40955602e929cb1193e78700f15af99
Fady Mondy <EngFadyMondy@gmail.com>
2023-02-07 18:18:09 +0200

    first commit 🔥

---
+++ README.md
@@ -0,0 +1,64 @@
+# Tomato forge
+
+Forge Server manager with Tomato Admin & Splade ready to use UI inside your app
+
+## Installation
+
+```bash
+composer require tomatophp/tomato-forge
+```
+after install your package please run this command
+
+```bash
+php artisan tomato-forge:install
+```
+
+## Publish Assets
+
+you can publish config file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-forge-config"
+```
+
+you can publish views file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-forge-views"
+```
+
+you can publish languages file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-forge-lang"
+```
+
+you can publish migrations file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-forge-migrations"
+```
+
+## Support
+
+you can join our discord server to get support [TomatoPHP](https://discord.gg/Xqmt35Uh)
+
+## Docs
+
+you can check docs of this package on [Docs](https://docs.tomatophp.com/plugins/laravel-package-generator)
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Security
+
+Please see [SECURITY](SECURITY.md) for more information about security.
+
+## Credits
+
+- [Tomatophp](mailto:info@3x1.io)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/TomatoForgeServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tomatophp\TomatoForge;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class TomatoForgeServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //Register generate command
+        $this->commands([
+           \Tomatophp\TomatoForge\Console\TomatoForgeInstall::class,
+        ]);
+ 
+        //Register Config file
+        $this->mergeConfigFrom(__DIR__.'/../config/tomato-forge.php', 'tomato-forge');
+ 
+        //Publish Config
+        $this->publishes([
+           __DIR__.'/../config/tomato-forge.php' => config_path('tomato-forge.php'),
+        ], 'tomato-forge-config');
+ 
+        //Register Migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+ 
+        //Publish Migrations
+        $this->publishes([
+           __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'tomato-forge-migrations');
+        //Register views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tomato-forge');
+ 
+        //Publish Views
+        $this->publishes([
+           __DIR__.'/../resources/views' => resource_path('views/vendor/tomato-forge'),
+        ], 'tomato-forge-views');
+ 
+        //Register Langs
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'tomato-forge');
+ 
+        //Publish Lang
+        $this->publishes([
+           __DIR__.'/../resources/lang' => app_path('lang/vendor/tomato-forge'),
+        ], 'tomato-forge-lang');
+ 
+        //Register Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+ 
+    }
+
+    public function boot(): void
+    {
+        //you boot methods here
+    }
+}
